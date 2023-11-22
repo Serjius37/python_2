0b6d8d5f32e397ae6bdcdc4106bbd23436e3ec83
Sergey <ushackov.serge@gmail.com>
2023-11-22 18:06:37 +0300

    Added ch. 6

---
+++ 062.08.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+Дополните приведенный код, используя срезы, чтобы он вывел все элементы кортежа countries, кроме двух последних и трех первых.
+'''
+
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+countries1 = countries[3:-2]
+print(countries1)
